b6324e7ec3eac8efbfe77f73b1b59a4773961880
Flavian <Flavianmohanraj@gmail.com>
2020-02-11 00:58:52 +0000

    added seeds to categories

---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,6 @@
+INSERT INTO categories (title) VALUES ('Books')
+INSERT INTO categories (title) VALUES ('Movies')
+INSERT INTO categories (title) VALUES ('Food')
+INSERT INTO categories (title) VALUES ('Products')
+INSERT INTO categories (title) VALUES ('Other')
+
