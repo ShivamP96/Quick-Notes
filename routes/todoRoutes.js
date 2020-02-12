34de370d98f14c0541beaee4d482a7c885f1cc25
cassandralin <cassandralin92@gmail.com>
2020-02-12 17:38:47 +0000

    connected db with modals

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -17,12 +17,28 @@ module.exports = (db) => {
     res.render("add");
   })
 
-  // router.get("/", (req, res) => {
-  //   res.render("index");
-  // });
+  router.get("/", (req, res) => {
+    const task = `
+      SELECT t.id task_id, c.id category_id, t.input, c.title title
+      FROM tasks AS t
+      INNER JOIN categories AS c ON c.id = t.category_id
+    `; //only select for books
+    db.query(task) //adding queries to new variable, they can all load at the same time
+      .then(data => { //data is the result of query, use data in templateVars
+        // TODO: Group your tasks by category
+        let templateVars = {tasks: data.rows}
+        console.log(templateVars.tasks.length);
+        res.render("index", templateVars)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message }); //if not pass error message
+      });
+  });
+
 
   router.post("/", (req, res) => {
-    console.log("YOU ARE ON THE POST /")
     if (!req.body.text) {
       res.status(400).json({error: 'invalid request: no data in POST body'});
       return;
@@ -38,3 +54,5 @@ module.exports = (db) => {
   })
   return router;
 };
+
+
