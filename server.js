607f33d3ca1e43aba13f6777489b8e9e562fa91b
Flavian <Flavianmohanraj@gmail.com>
2020-02-14 00:16:07 +0000

    added homepage

--- server.js
+++ server.js
@@ -53,13 +53,13 @@ app.use("/todo", todoRoutes(db));
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.get("/login", (req, res) => {
-  res.render("login");
-});
+// app.get("/login", (req, res) => {
+//   res.render("login");
+// });
 
-app.get("/register", (req, res) => {
-  res.render("register");
-});
+// app.get("/register", (req, res) => {
+//   res.render("register");
+// });
 
 app.post("/register", async(req,res) =>{
   console.log("Hey we connected to /register POST")
